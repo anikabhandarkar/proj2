3c5be07f3daca68ce50098eb0f89622d46afa360
aleenabasil <137114907+aleenabasil@users.noreply.github.com>
2025-02-17 15:06:49 -0800

    Update DSVWriter.cpp

--- src/DSVWriter.cpp
+++ src/DSVWriter.cpp
@@ -1,8 +1,8 @@
 #include "DSVWriter.h"
 #include "DataSink.h"
 
-
-struct CDSVWriter::SImplementation{
+struct CDSVWriter::SImplementation
+{
     std::shared_ptr<CDataSink> Sink;
     char Delimiter;
     bool QuoteAll;
@@ -14,44 +14,78 @@ struct CDSVWriter::SImplementation{
 // specifies the delimiting character, and quoteall specifies if all values
 // should be quoted or only those that contain the delimiter, a double quote,
 // or a newline
-CDSVWriter::CDSVWriter(std::shared_ptr< CDataSink > sink, char delimiter, bool quoteall = false)
-    : DImplementation(std::make_unique<SImplementation>(sink, delimiter, quoteall)){}
+CDSVWriter::CDSVWriter(std::shared_ptr<CDataSink> sink, char delimiter, bool quoteall = false)
+    : DImplementation(std::make_unique<SImplementation>(sink, delimiter, quoteall)) {}
 
 // Destructor for DSV writer
 CDSVWriter::~CDSVWriter() = default;
+
 // Returns true if the row is successfully written, one string per column
 // should be put in the row vector
-bool CDSVWriter::WriteRow(const std::vector< std::string > &row){
-    if(!DImplementation || !DImplementation->Sink) return false;
+bool CDSVWriter::WriteRow(const std::vector<std::string> &row)
+{
+    if (!DImplementation || !DImplementation->Sink)
+    {
+        return false;
+    }
 
-    auto& sink = DImplementation->Sink;
+    if (row.empty())
+    {
+        return DImplementation->Sink->Put('\n');
+    }
+
+    auto &sink = DImplementation->Sink;
     char delimiter = DImplementation->Delimiter;
-    bool quote = DImplementation->QuoteAll;
+    bool quoteAll = DImplementation->QuoteAll;
 
-    for(size_t i = 0; i < row.size(); ++i){
-        bool nequote = quote || row[i].find(delimiter) != std::string::npos ||
-            row[i].find('"') != std::string::npos;
+    for (size_t i = 0; i < row.size(); ++i)
+    {
+        const auto &field = row[i];
 
-        if(nequote){
-            if(!sink->Put('"')) return false;;
-            for(char ch : row[i]){
-                if(ch == '"'){
-                    if(!sink->Put('"') || !sink->Put('"')) return false;
-                }else{
-                    if(!sink->Put(ch)) return false;
+        bool needsQuote = quoteAll ||
+                          field.find(delimiter) != std::string::npos ||
+                          field.find('"') != std::string::npos ||
+                          field.find('\n') != std::string::npos;
+
+        if (needsQuote)
+        {
+            if (!sink->Put('"'))
+                return false;
+
+            for (char ch : field)
+            {
+                if (ch == '"')
+                {
+                    if (!sink->Put('"'))
+                        return false;
+                    if (!sink->Put('"'))
+                        return false;
+                }
+                else
+                {
+                    if (!sink->Put(ch))
+                        return false;
                 }
             }
-            if(!sink->Put('"')) return false;
-        } else{
-            for (char ch : row[i]){
-                if(!sink->Put(ch)) return false;
+
+            if (!sink->Put('"'))
+                return false;
+        }
+        else
+        {
+            for (char ch : field)
+            {
+                if (!sink->Put(ch))
+                    return false;
             }
         }
-        if(i < row.size() - 1){
-            if(!sink->Put(delimiter)) return false;
-        }      
-        
+
+        if (i < row.size() - 1)
+        {
+            if (!sink->Put(delimiter))
+                return false;
+        }
     }
+
     return sink->Put('\n');
 }
-
