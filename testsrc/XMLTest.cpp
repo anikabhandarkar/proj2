a0d385f5797755d27eaa49db45853402cd5c5188
anikabhandarkar <150579417+anikabhandarkar@users.noreply.github.com>
2025-02-17 22:08:43 -0800

    Update XMLTest.cpp

--- testsrc/XMLTest.cpp
+++ testsrc/XMLTest.cpp
@@ -1,10 +1,9 @@
+#include <gtest/gtest.h> 
 #include "XMLReader.h"
 #include "XMLWriter.h"
 #include "StringDataSource.h"
 #include "StringDataSink.h"
-#include <gtest/gtest.h> 
 
-// define a test case for basic XML read and write operations
 TEST(XMLTest, BasicReadWrite) {
     // create a data source from a string containing XML data
     std::shared_ptr<CStringDataSource> src = std::make_shared<CStringDataSource>("<tag>data</tag>");
@@ -12,20 +11,20 @@ TEST(XMLTest, BasicReadWrite) {
     // create a data sink to capture output from the XML writer
     std::shared_ptr<CStringDataSink> sink = std::make_shared<CStringDataSink>();
 
-    // instantiate the XML reader with the string data source
+    // initialize XML reader w/ the string data
     CXMLReader reader(src);
 
-    // instantiate the XML writer with the string data sink
+    // initialize XML writer w/ the string data sink
     CXMLWriter writer(sink);
 
     // define an XML entity to store data read from the XML reader
     SXMLEntity entity;
 
-    // continuously read from the XML reader until the end of the data is reached
+    // read from the XML reader until the end of data is reached
     while (!reader.End()) {
-        // attempt to read an entity from the XML stream
+        // attempt to read entity from XML stream
         if (reader.ReadEntity(entity)) {
-            // if an entity is successfully read, write it to the XML writer
+            // if an entity is successfully read, write to the XML writer
             writer.WriteEntity(entity);
         }
     }
